fdb8fce907058b88877d8c398c11417b66acbb4a
igorraknorwid <igorrakfed@gmail.com>
2023-08-01 12:22:43 +0200

    titl3 3

--- src/utils/constants/colors.ts
+++ src/utils/constants/colors.ts
@@ -2,7 +2,7 @@ export const BAGROUNDS = {
   ACTIVE: "bg-blue-700",
   ACTIVE_BORDER: "border-blue-700",
   PASSIVE: "bg-white",
-  PRIME: "bg-blue-700",
+  PRIME: "bg-green-700",
   SECONDARY: "bg-blue-200",
   COVER: "bg-gray-200",
 };
