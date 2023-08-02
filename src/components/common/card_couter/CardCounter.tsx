97f2915295dc54dcf2273495089fc0faf4cb6b2e
igorraknorwid <igorrakfed@gmail.com>
2023-08-02 14:12:09 +0200

    hello

--- src/components/common/card_couter/CardCounter.tsx
+++ src/components/common/card_couter/CardCounter.tsx
@@ -15,7 +15,7 @@ function CardCounter({ cards }: ICardCounter) {
     <>
       {cards && (
         <div className='text-center text-lg my-2'>
-          Iłość kart: {cards.length}
+          Liczba kart: {cards.length}
         </div>
       )}
     </>
