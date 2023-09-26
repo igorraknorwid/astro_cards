f5a7b9322ed519e5016c7f97202dcdf725b94914
igorraknorwid <igorrakfed@gmail.com>
2023-09-26 10:56:07 +0200

    all card counter

---
+++ src/components/allcards/AllProjectCards.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import client from "../../api/sanityClient";
+import Spinner from "../common/spinner/Spinner";
+import { groq_params } from "../../api/groq/groq";
+import { ICard } from "../../types/card";
+
+function AllProjectCards() {
+  const [data, setData] = React.useState<ICard[] | null>(null);
+  const [isError, setIsError] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const query = `*[_type == "card"]{_id}`;
+        const result = await client.fetch<ICard[]>(query);
+        setData(result);
+      } catch (error) {
+        setIsError(true);
+        console.error("Error fetching data from Sanity:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data)
+    return (
+      <div className='mx-2 md:mx-[5%] flex justify-center items-center mt-[10%]'>
+        <Spinner />
+      </div>
+    );
+  if (isError) return <div>"Error fetching data from Sanity:"</div>;
+  return (
+    <div className='py-5 mx-2 md:mx-[5%] flex flex-col items-center md:items-start gap-y-4'>
+      <div className='text-lg font-thin'>
+        {" "}
+        Łączna liczba kart bibliograficznych:{data.length + 1}
+      </div>
+    </div>
+  );
+}
+
+export default AllProjectCards;
